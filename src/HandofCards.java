bf63e5ea6bda03ca0afaed93d5b8c0c5a6d877dd
khushah <khevna92@gmail.com>
2015-07-30 19:04:24 +0530

    Adding class design

---
+++ src/HandofCards.java
@@ -0,0 +1,16 @@
+
+public class HandofCards {
+
+	int[][] S,C,D,H;
+
+	public static void HandParse(){}
+	
+	public static void updateCards(){}
+	
+	public static void isRun(){}
+	
+	public static void isSet(){}
+	
+	public static void isNaturalSet(){}
+	
+};
---
+++ src/Rummy.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class Rummy {
+
+	public static ArrayList<String> generateHands(){
+		ArrayList<String> hand = new ArrayList<String>();
+		return hand;
+	}
+	
+	public static void main(String[] args){
+		return;
+	}
+}
