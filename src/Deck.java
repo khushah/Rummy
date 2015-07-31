c87cc4644ec358d2baa8e924d1319eddbd0d63c6
asrayousuf <asrayousuf28@gmail.com>
2015-07-31 09:59:19 +0530

    Changed rank type

--- src/Deck.java
+++ src/Deck.java
@@ -6,15 +6,15 @@ public class Deck {
 	
 	    ArrayList<Card> cards = new ArrayList<Card>();
 
-	    String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-	    String[] suit = {"C", "S", "D", "H"};
+	    int[] ranks = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+	    char[] suits = {'S', 'H', 'C', 'D'};
 
 	    public Deck(){
 	    	for(int k=0 ; k < 3 ; k++){
-	    	this.cards.add(new Card("Joker",""));
-	        for (int i = 0; i<suit.length; i++) {
-	            for(int j=0; j<values.length; j++){
-	                this.cards.add(new Card(suit[i],values[j]));
+	    	this.cards.add(new Card(14,' '));
+	        for (int i = 0; i<suits.length; i++) {
+	            for(int j=0; j<ranks.length; j++){
+	                this.cards.add(new Card(ranks[j],suits[i]));
 	            }
 	        }
 	    	}
