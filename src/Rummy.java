7ad2771f3b69e1724b5122a3487d5a6c6650b872
asrayousuf <asrayousuf28@gmail.com>
2015-07-31 09:04:37 +0530

    Added main class

--- src/Rummy.java
+++ src/Rummy.java
@@ -2,13 +2,23 @@ import java.util.ArrayList;
 
 
 public class Rummy {
+	public void generateHand(ArrayList<Card> hand){
+		HandOfCards handOfCards =  new HandOfCards();
+		for(int i=0;i<13;i++){
+			handOfCards.parseHand(hand.get(i).getSuit(),hand.get(i).getValue());
+		}
+		
+	}
 
-	public static ArrayList<String> generateHands(){
-		ArrayList<String> hand = new ArrayList<String>();
-		return hand;
+	    public static void main(String[] args){
+	        Deck deck = new Deck();
+	        ArrayList<Card> result = deck.getDeck();
+	        Rummy rummy = new Rummy();
+	        rummy.generateHand(result);
+	      /*  for(int i=0;i<13;i++){
+	        System.out.println(result.get(i).getSuit()+" "+result.get(i).getValue());       
+	        }*/
+
 	}
-	
-	public static void main(String[] args){
-		return;
-	}
+
 }
